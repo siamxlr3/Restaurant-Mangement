12022109e1b9b8405aa61a961620a409639a953a
siamxlr3 <siamxlr3@gmail.com>
2026-06-29 01:03:18 +0600

    fixed main.jsx folder issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './store'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root')
+if (!container) throw new Error('Root container not found')
+
+createRoot(container).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
